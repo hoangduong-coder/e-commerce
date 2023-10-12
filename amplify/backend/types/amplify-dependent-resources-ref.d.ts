dfd6fd70e8ba0a54ffa63199249c225e47a5162a
Hoang Duong <hoangduongphantri@gmail.com>
2023-10-12 18:27:39 +0300

    feat: Initialize AWS Backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awssignin": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
