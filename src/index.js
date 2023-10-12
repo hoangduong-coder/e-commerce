d1738473240d299e4a603398ddbbd4f46207147d
Hoang Duong <hoangduongphantri@gmail.com>
2023-10-13 01:02:24 +0300

    Configure application with AWS Amplify

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import './index.css';
 
 import {ThemeProvider} from '@emotion/react';
 import {createTheme} from '@mui/material';
+import {Amplify} from 'aws-amplify';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import config from './aws-exports';
 
 const theme = createTheme ({
   typography: {
     fontFamily: ['"Montserrat"', 'sans-serif'].join (','),
   },
 });
+
+Amplify.configure (config);
 
 const root = ReactDOM.createRoot (document.getElementById ('root'));
 root.render (
