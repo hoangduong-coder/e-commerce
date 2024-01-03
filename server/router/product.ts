d4f82b6dc1552625a416cfa1c3e3c6490549e3f2
Hoang Duong <hoangduongphantri@gmail.com>
2024-01-03 23:49:23 +0200

    First router creation

--- server/router/product.ts
+++ server/router/product.ts
@@ -1,9 +1,27 @@
-import { Router } from "express";
+import Product from "../schema/product"
+import { Router } from "express"
 
 const router = Router()
-router.get("/", (_req, _res) => {
-  console.log("Product list")
+
+router.get("/", async (_req, res) => {
+  const products = await Product.find({})
+  return res.json(products)
 })
-router.get("/:category", (req, _res) => {
-  console.log(`List of ${req.params.category}`)
+
+router.get("/:category", async (req, res) => {
+  const products = await Product.find({ productType: req.params.category })
+  if (products) {
+    return res.json(products)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.get("/:id", async (req, res) => {
+  const selectedProduct = await Product.findById(req.params.id)
+  if (selectedProduct) {
+    return res.json(selectedProduct)
+  } else {
+    res.status(404).end()
+  }
 })
