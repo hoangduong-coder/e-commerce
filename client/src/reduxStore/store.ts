58bc12ce8d28a6bcd7ec89ce39d1af93706b5814
Hoang Duong <hoangduongphantri@gmail.com>
2024-02-19 18:11:34 +0200

    Add redux settings

--- client/src/reduxStore/store.ts
+++ client/src/reduxStore/store.ts
@@ -1,9 +1,13 @@
-// import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import authStore from "./authSlice";
 
-// export const store = configureStore({
-//   reducer: {
-//     products: productStore,
-//     users: userStore,
-//   }
-  
-// })
+export const store = configureStore({
+  reducer: {
+    // products: productStore,
+    // users: userStore,
+    auth: authStore
+  }
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
