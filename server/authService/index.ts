c53742aa74f03bfbb5f6c996ced41604d9806a4c
hoangduong-coder <hoangduongphantri@gmail.com>
2024-04-20 00:52:30 +0300

    Create auth microservice

--- server/authService/index.ts
+++ server/authService/index.ts
@@ -4,10 +4,8 @@ import { MONGO_URL, PORT } from "./helper/utils"
 import cors from "cors"
 import express from "express"
 import mongoose from "mongoose"
-import loginRouter from "./controller/auth"
-import orderRouter from "./controller/order"
-import productRouter from "./controller/product"
-import userRouter from "./controller/user"
+import loginRouter from "./router/auth"
+import userRouter from "./router/user"
 
 const app = express()
 
@@ -20,14 +18,12 @@ app.use(cors())
 app.use(express.static("dist"))
 app.use(express.json())
 
-app.use("/api/products", productRouter)
 app.use("/api/users", userRouter)
 app.use("/api/auth", loginRouter)
-app.use("/api/orders", orderRouter)
 
 app.use(unknownEndpoint)
 app.use(handleError)
 
 app.listen(PORT, () => {
-  console.log("Welcome to my new e-commerce")
+  console.log("New e-commerce authentication microservice is ready")
 })
